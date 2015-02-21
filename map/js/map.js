26f07610ffb314f3f89f1be60ee32cb359f9b5bc
Michal Zimmermann <zimmicz@gmail.com>
2015-02-21 20:06:42 +0100

    Awesome markers

--- map/js/map.js
+++ map/js/map.js
@@ -1,11 +1,6 @@
 var M = (function(my) { "use strict";
-    var basemap, cluster,
-        colors = [
-            "#B02B2C", "#6BBA70", "#3F4C6B", "#356AA0", "#D01F3C", "#73880A",
-            "#C79810", "magenta", "grey", "ivory", "pink", "maroon", "navy"
-        ],
-        json, map, styles, vozejkmap;
-    my.version = "0.0.2";
+    var basemap, cluster, json, map, styles, vozejkmap;
+    my.version = "0.0.3";
 
     basemap = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
 
@@ -15,6 +10,78 @@ var M = (function(my) { "use strict";
         zoom: 8,
     });
 
+    styles = {
+        "1": { // culture
+            "icon": "video-camera",
+            "markerColor": "blue",
+            "prefix": "fa"
+        },
+        "2": { // sports
+            "icon": "ios7-football",
+            "markerColor": "darkgreen",
+            "prefix": "ion"
+        },
+        "3": { // institutions
+            "icon": "institution",
+            "markerColor": "gray",
+            "prefix": "fa"
+        },
+        "4": { // food and drink
+            "icon": "glass",
+            "markerColor": "orange",
+            "prefix": "fa"
+        },
+        "5": { // acommodation
+            "icon": "bed",
+            "prefix": "fa"
+        },
+        "6": { // health care
+            "icon": "heartbeat",
+            "markerColor": "red",
+            "prefix": "fa"
+        },
+        "7": { // other
+            "icon": "more",
+            "prefix": "ion"
+        },
+        "8": { // transportation
+            "icon": "road",
+            "markerColor": "gray",
+            "prefix": "glyphicon"
+        },
+        "9": { // public toilets
+            "icon": "venus-mars",
+            "iconColor": "black",
+            "markerColor": "white",
+            "prefix": "fa"
+        },
+        "10": { // gas station
+            "icon": "tint",
+            "markerColor": "darkpurple",
+            "prefix": "fa"
+        },
+        "11": { // shopping
+            "icon": "ios7-cart",
+            "markerColor": "green",
+            "prefix": "ion"
+        },
+        "12": { // banks, ATMs
+            "icon": "social-usd",
+            "markerColor": "green",
+            "prefix": "ion"
+        },
+        "13": { // parking lots
+            "icon": "heart",
+            "markerColor": "gray",
+            "prefix": "ion"
+        },
+        "14": { // Škoda auto (wtf?!)
+            "icon": "car",
+            "markerColor": "gray",
+            "prefix": "fa"
+        }
+    };
+
     json = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.bindPopup(L.Util.template("<h1>{title}</h1><p>{description}</p>", {
@@ -23,15 +90,7 @@ var M = (function(my) { "use strict";
             }));
         },
         pointToLayer: function(feature, position) {
-            var style = {
-                "color": "#444",
-                "fillColor": colors[parseInt(feature.properties.location_type)],
-                "fillOpacity": 1,
-                "radius": 5,
-                "weight": 1
-            }
-
-            return L.circleMarker(position, style);
+            return L.marker(position, {icon: L.AwesomeMarkers.icon(styles[feature.properties.location_type])});
         }
     });
 
